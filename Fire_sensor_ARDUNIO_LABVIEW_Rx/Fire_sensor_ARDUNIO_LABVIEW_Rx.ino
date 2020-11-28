d57c5acd1e6d5127ccc501aa045e970bd1b48e01
Dr. Rajesh Singh <75157838+RajeshSingh001@users.noreply.github.com>
2020-11-28 13:31:53 +0530

    Transmitter and Receiver Code in IDE, Proteus Model, LabVIEW GUI

---
+++ Fire_sensor_ARDUNIO_LABVIEW_Rx/Fire_sensor_ARDUNIO_LABVIEW_Rx.ino
@@ -0,0 +1,37 @@
+// the setup routine runs once when you press reset:
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup()
+{
+  // initialize serial communication at 9600 bits per second:
+  lcd.begin(16,2);
+  Serial.begin(9600);
+}
+
+// the loop routine runs over and over again forever:
+void loop() 
+{  
+  int FSR1 = Serial.read();;
+   // check if the pushbutton is pressed.
+  // if it is, the buttonState is HIGH:
+  if(FSR1 == 10) 
+  { 
+   lcd.clear();     
+   Serial.print("FSR1:");
+   Serial.println(FSR1);
+   lcd.setCursor(0,0);
+   lcd.print("Fire Detected");
+   delay(50);
+  } 
+   else if(FSR1 == 20) 
+  {
+   lcd.clear();
+   Serial.print("FSR1:");
+   Serial.println(FSR1);
+   lcd.setCursor(0,1);
+   lcd.print("Fire Not Detected");
+   delay(50); 
+  }
+ 
+}
---
+++ Fire_sensor_ARDUNIO_LABVIEW_Tx/Fire_sensor_ARDUNIO_LABVIEW_Tx.ino
@@ -0,0 +1,46 @@
+// the setup routine runs once when you press reset:
+const int buttonPin1 = 10; 
+  // the number of the pushbutton pin
+//const int ledPin =  13;
+int buttonState1=0;
+
+
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup()
+{
+  // initialize serial communication at 9600 bits per second:
+  lcd.begin(16,2);
+  Serial.begin(9600);
+  pinMode(buttonPin1, INPUT);
+ 
+}
+
+// the loop routine runs over and over again forever:
+void loop() 
+{  
+  buttonState1 = digitalRead(buttonPin1);
+    // check if the pushbutton is pressed.
+  // if it is, the buttonState is HIGH:
+  if(buttonState1 == HIGH)
+  { 
+   lcd.clear();     
+   int FSR1=10;
+   Serial.write(FSR1); 
+   lcd.setCursor(0,0);
+   lcd.print("FIRE DETECTED");
+   delay(50);
+  } 
+  
+  else
+  {
+    lcd.clear();     
+   int FSR1=20;
+   Serial.write(FSR1); 
+   lcd.setCursor(0,0);
+   lcd.print("FIRE NOT DETECTED");
+   delay(50);
+  }
+  
+  }
